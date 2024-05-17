6802112a9f331051311da88f48ce4d27a2aa121e
ArachnidArchitect <proffesionalworkingstuff@gmail.com>
2024-05-17 16:28:40 +0200

    all requirements met

---
+++ script.js
@@ -0,0 +1,13 @@
+// (0°C × 9/5) + 32 = 32°F
+
+let input = document.querySelector('input');
+let button = document.querySelector('button');
+let result = document.querySelector('h1');
+
+button.addEventListener('click', ()=>{
+    intInput = +input.value
+    calculation = ((intInput * 9/5) + 32).toString()
+//   console.log((intInput * 9/5) + 32).toString()
+    result.innerText = (input.value + '°C = '+calculation + '°F')
+})
+
